6a85773c7c2d43046d2713635f1d63a2421f90d9
Nikhil-0200 <nikhiltiwari0200@gmail.com>
2023-12-13 05:47:20 +0530

    Phonpe Ass. Done

--- script.js
+++ script.js
@@ -1,162 +1,187 @@
-// initiating Transaction 
-// Connecting Securely
+    // initiating Transaction 
+    // Connecting Securely
 
-let textBox = document.getElementById("text");
-let msg = document.getElementById("msg");
+    let textBox = document.getElementById("text");
+    let msg = document.getElementById("msg");
 
 
-function leftArrow(){
-    let leftArrow = document.querySelector("#head-l > i");
-    leftArrow.addEventListener("click", function(){
-        gsap.to("#m-body",{
-            y: 0,
-            opacity: 0,
-        })
-    
-        gsap.to("#curr",{
-            x: 0,
-            opacity: 0,
-        })
-    
-        gsap.to("#text",{
-            opacity: 0,
-        })
-    
-    })
-    
-}
-function InputAnimation(){
-    textBox.addEventListener("input", function(){
-        let textData = textBox.value;
-        
-        if(textData.trim() !== ""){
-            gsap.to("#m-body",{
-                y: -0,
-                opacity: 1,
-            })
-            gsap.to("#curr",{
-                x: 10,
-                opacity: 1,
-            })
-            gsap.to("#file",{
-                x: 0,
-                opacity: 1,
-            })
-    
-            gsap.to("#foot-icon",{
-                opacity: 0,
-            })
-            gsap.to("#text",{
-                width: "45%"
-            })
-            gsap.to("#foot-icon2",{
-                opacity: 1,
-            })
-            
-        }
-        else{
+    function leftArrow(){
+        let leftArrow = document.querySelector("#head-l > i");
+        leftArrow.addEventListener("click", function(){
             gsap.to("#m-body",{
                 y: 0,
                 opacity: 0,
             })
-    
-            gsap.to("#curr",{
-                // x: -10,
-                opacity: 0,
-            })
-            gsap.to("#foot-icon",{
-                opacity: 1,
-            })
-    
-            gsap.to("#file",{
-                opacity: 0,
-            })
-    
-            gsap.to("#foot-icon2",{
-                opacity: 0,
-            })
-        }
-    
-       
-    
         
-    })
-}
+            gsap.to("#curr",{
+                x: 0,
+                opacity: 0,
+            })
+        
+            gsap.to("#text",{
+                opacity: 0,
+            })
+        
+        })
+        
+    }
+    function InputAnimation(){
+        textBox.addEventListener("input", function(){
+            let textData = textBox.value;
+            
+            if(textData.trim() !== ""){
+                gsap.to("#m-body",{
+                    y: -0,
+                    opacity: 1,
+                })
+                gsap.to("#curr",{
+                    x: 10,
+                    opacity: 1,
+                })
+                gsap.to("#file",{
+                    x: 0,
+                    opacity: 1,
+                })
+        
+                gsap.to("#foot-icon",{
+                    opacity: 0,
+                })
+                gsap.to("#text",{
+                    width: "35%"
+                })
+                gsap.to("#foot-icon2",{
+                    opacity: 1,
+                })
+                
+            }
+            else{
+                gsap.to("#m-body",{
+                    y: 0,
+                    opacity: 0,
+                })
+        
+                gsap.to("#curr",{
+                    // x: -10,
+                    opacity: 0,
+                })
+                gsap.to("#foot-icon",{
+                    opacity: 1,
+                })
+        
+                gsap.to("#file",{
+                    opacity: 0,
+                })
+        
+                gsap.to("#foot-icon2",{
+                    opacity: 0,
+                })
+            }
+        
+        
+        
+            
+        })
+    }
 
-function payAction(){
-    let payBtn = document.getElementById("pay");
-    
-    payBtn.addEventListener("click", function(){
-        let textData = text.value;
-        let msgDta = msg.textContent;
-        CreatePromise(textData);
+    function payAction(){
+        let payBtn = document.getElementById("pay");
+        
+        payBtn.addEventListener("click", function(){
+            let textData = textBox.value;
+            let msgDta = msg.textContent;
 
-        setTimeout(() => {
-            let Blur = document.getElementById("overLay");
-        Blur.style.display = "block";
+            setTimeout(() => {
+                let Blur = document.getElementById("overLay");
+            Blur.style.display = "block";
 
-        loadingPromise(msgDta);
 
-         
+            loadingPromise(msgDta);
+
         setTimeout(() => {
             let overLay2 = document.getElementById("overLay2");
             overLay2.style.display = "block";
-        }, 3000);
-        
-    }, 1000);    
 
-    
-    })
-}
+            setTimeout(() => {
+                let overLay3 = document.getElementById("overLay3");
+                let overLay4 = document.getElementById("overLay4");
+                let numericValue = Number(textData);
+                if(!isNaN(numericValue)){
+                overLay3.style.display = "block";
+                    
+                }
+                else{
+                    overLay3.style.display = "none";
+                    overLay4.style.display = "block";
 
 
-function loadingPromise(msgDta){
-    let load = new Promise(function(res, rej){
-        setTimeout(function(){
-            if(msgDta){
-                res("Connecting Securely")
-            }
-        },2000)
-    })
+                }
+            }, 4000);
+        }, 4000);
+        }, 1000);   
 
-load.then(function(ans){
-    let msg = document.getElementById("msg");
-    msg.textContent = ans.value;
-    console.log(ans);
-    msg.append(ans)
-})
+        CreatePromise(textData);
 
-}
+        })
+    }
 
-function CreatePromise(textData){
 
-    if (!textData) {
-        return Promise.reject("Enter A Valid Response");
-      }
-      
-    let Mypromise = new Promise(function(res, rej){
-        setTimeout(function(){
-            let numericValue = Number(textData);
-            if(!isNaN(numericValue)){
-                res(numericValue);
-            }
-            else{
-                rej("Enter A Valid Response")
-            }
-        },1000)
-    })
+    function loadingPromise(msgDta){
+        let load = new Promise(function(res, rej){
+            setTimeout(function(){
+                if(msgDta){
+                    res("Connecting Securely")
+                }
+            },2000)
+        })
 
-    Mypromise.then(function(ans){
+    load.then(function(ans){
+        let msg = document.getElementById("msg");
+        msg.textContent = ans.value;
         console.log(ans);
+        msg.append(ans)
     })
-    Mypromise.catch(function(err){
-        console.log(err);
-    })
-} 
+
+    }
+
+    function CreatePromise(textData){
+
+        if (!textData) {
+            return Promise.reject("Enter A Valid Response");
+        }
+        
+        let Mypromise = new Promise(function(res, rej){
+            setTimeout(function(){
+                let numericValue = Number(textData);
+                if(!isNaN(numericValue)){
+                    res(numericValue);
+                }
+                else{
+                    rej("Enter A Valid Response.")
+            
+                }
+            },1000)
+        })
+
+        Mypromise.then(function(ans1){
+            let payCmt = document.querySelector("#pay-cmt span");
+            payCmt.textContent = ans1;
+        })
+        Mypromise.catch(function(err){
+            let payCmt1 = document.querySelector("#pay-cmt1 h3");
+            payCmt1.textContent = err;
+        })
+    } 
+
+    // gsap.to(".loader",{
+    //     display: "block",
+    //     y: 10,
+    // })
 
 
-loadingPromise();
-CreatePromise();
-payAction();
-leftArrow();
-InputAnimation();
+
+    loadingPromise();
+    CreatePromise();
+    payAction();
+    leftArrow();
+    InputAnimation();
+
